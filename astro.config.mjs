eaf033b0fd0d1b96c844b6df30bc1610ca28f05a
Nicholas Gillespie <contact@nicholasgillespie.com>
2024-07-02 16:54:14 +0200

    chore: Update trailingSlash option in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,5 @@ export default defineConfig({
   integrations: [purgecss()],
   site: "https://nicholasgillespie.github.io",
   base: "/art-gallery/",
-  trailingSlash: "never",
+  trailingSlash: "ignore",
 });
